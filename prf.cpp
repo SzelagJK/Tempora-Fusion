2c25a69b2d06739449f41a58fde59898985ffe4d
Jakub Szelag <jakub.szelag2@newcastle.ac.uk>
2026-02-10 20:03:38 +0000

    added some missed comments (one, but important for my sanity)

--- prf.cpp
+++ prf.cpp
@@ -1,5 +1,6 @@
 #include "prf.h"
 
 // NOTES FOR TOMORROW: implement prf with arbitrary output size, refer to notes made before, try to reuse alerady implemented functions (no point of reimplementing encoding helpers, unless necessary)
+// check where exactly the 2nd PRF would be used, would be useful to know what lenght will it be, refer to the other paper
 // implement commitment functions, with that, hashes (shouldn't be too bad)
 // test both
